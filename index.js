915b3fe0aa656dfe7e216d5324b56d071072f85b
Kushantha Sajith <kushantha@linearsix.com>
2023-11-08 12:41:22 +0530

    Complete Project

--- index.js
+++ index.js
@@ -17,33 +17,59 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course){
-        res.status(404).send('The course with the given id was not found.');
-    }
+    if(!course) return res.status(404).send('The course with the given id was not found.');
     res.send(course);
 });
 
 
 app.post('/api/courses', (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-    const result = schema.validate({name: req.body.name});
+    const result = validateCourse(req.body);
+    const {error} = validateCourse(req.body);
     console.log(result);
-    if(result.error){
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    if(error) return res.status(400).send(error.details[0].message);
+
     const course = {
         id: courses.length + 1, 
         name: req.body.name
     };
+
     courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given id was not found.');
+
+    const result = validateCourse(req.body);
+    const {error} = validateCourse(req.body);
+    console.log(result);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(course);
+}
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given id was not found.');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
     res.send(course);
 });
 
